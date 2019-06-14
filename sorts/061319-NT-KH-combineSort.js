e3cf24531269c199c2fe600aafe15246d71ee6e8
Kent Hervey <kent.hervey@gmail.com>
2019-06-14 11:17:33 -0500

    combine complete

--- sorts/061319-NT-KH-combineSort.js
+++ sorts/061319-NT-KH-combineSort.js
@@ -5,28 +5,27 @@
 let sample1 = [1, 3, 5, 7, 9];
 //             0  1  2  3  4
 
-let sample2 = [2, 4, 6, 8, 10];
-//             0  1  2  3  4  5
+let sample2 = [1, 2, 4, 6, 8, 10, 15];
+//             0  1  2  3  4  5    6
 
 function combineArrs(inArray1, inArray2) {
-    //loop through inArray1 then use inner array to scan through inArray2
 
-    let index1 = 0;
-    let index2 = 0;
-
-    //just looking inArray2 when i=2, and the value is 6
-    let i = 2;
-
-    for (i = 0; i < inArray2.length; i++){
-        for (j = 0; j < inArray1.length; j++) {
-            if ((inArray1[j]) < inArray2[i] && (inArray1[j + 1] > inArray2[i])) {
-                stickItAfter(j, inArray1, inArray2[i]);
-                console.log(j)
-            }
+    let i = 0;
+    let j = 0
+    while (i<inArray1.length && j<inArray2.length) {
+        if (inArray2[j] < inArray1[i]) {
+            stickItAfter(i - 1, inArray1, inArray2[j]);
+            j++;
         }
+        else {
+            i++;
+        }
+        }
+    while (j < inArray2.length) {
+        inArray1.push(inArray2[j]);
+        j++;
     }
     return inArray1;
-
 } 
 
     function stickItAfter(indexBefore, inArray, inValue) {
