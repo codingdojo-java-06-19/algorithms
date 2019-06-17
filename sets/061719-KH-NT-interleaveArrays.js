cf26c039d28e28a2070d5bbc3111f5b89d79d505
Kent Hervey <kent.hervey@gmail.com>
2019-06-17 10:59:42 -0500

    first commit interleave

---
+++ sets/061719-KH-NT-interleaveArrays.js
@@ -0,0 +1,32 @@
+// Interleave Arrays
+
+// Given two unsorted arrays, create a new array containing the elements of both, resulting in an unsorted merge of all values. When populating the new array alternate (interleave) values between the two arrays until one is exhausted, then include all of the other. Example: given [77,22,11,22] and [2,6,7,2,6,2] , return [77,2,22,6,11,7,22,2,6,2] .
+
+
+const sample1 = [77, 22, 11, 22];
+    
+const sample2 = [2,  6,  7,  2,  6,  2];
+
+function interleaveArrays(inArray1, inArray2) {
+
+    let newArray = [];
+    let i = 0;
+    let shorterArray = inArray2.length < inArray1.length? inArray2 : inArray1;
+    let longerArray = inArray2.length > inArray1.length? inArray2 : inArray1;
+    while (i<shorterArray.length) {
+        //alternate pushes i
+        newArray.push(inArray1[i], inArray2[i]);
+        i++
+        console.log(newArray);
+    } // end while checking for end of arrays
+    //put remainder of longer array in
+    for (let i = shorterArray.length; i < longerArray.length; i++) {
+        newArray.push(longerArray[i]);
+    }
+    return newArray;
+
+
+} // end interleaveArrays
+
+result = interleaveArrays(sample1, sample2);
+
